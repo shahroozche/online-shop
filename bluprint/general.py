0b3c7b426ab5c56d8c8bda780c3e536980375cde
Shahrooz Chegini <shahroozche32@gmail.com>
2025-06-12 20:23:47 +0330

    product page

--- bluprint/general.py
+++ bluprint/general.py
@@ -7,9 +7,16 @@ app = Blueprint('general',__name__)
 #main page address
 @app.route('/')
 def main():
-    products = Product.query.all()
+    products = Product.query.filter(Product.active == 1).all()
 
     return render_template("main.html",products=products)
+
+
+#main product page address
+@app.route('/products/<id>/<name>')
+def product(id,name):
+    product = Product.query.filter(Product.id == id).filter(Product.name == name).filter(Product.active == 1).first_or_404()
+    return render_template("product.html", product = product )
 
 #about us page address
 @app.route('/about')
