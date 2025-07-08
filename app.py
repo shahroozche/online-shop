59198dd51c5d72117f23c0e59a1f876f81aeec0e
Shahrooz Chegini <shahroozche32@gmail.com>
2025-07-08 20:49:24 +0330

    register page

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from bluprint.admin import app as admin
 from bluprint.users import app as users
 import config
 import extensions
+from flask_login import LoginManager
+from models.user import User
 
 app = Flask(__name__)
 app.register_blueprint(general)
@@ -12,6 +14,13 @@ app.register_blueprint(admin)
 app.register_blueprint(users)
 
 csrf = CSRFProtect(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 
 app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
 app.config["SECRET_KEY"] = config.SECRET_KEY
--- bluprint/users.py
+++ bluprint/users.py
@@ -1,10 +1,31 @@
-from flask import Blueprint
-import models.user 
+from flask import Blueprint,render_template,request,redirect
+from flask_login import login_user
+from models.user import *
+from passlib.hash import sha256_crypt
+from extensions import db
 
 app = Blueprint('users',__name__)
 
-#user page address
-@app.route('/user')
-def user():
-    return 'user page'
+#user login page address
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if request.method == "GET":
+        return render_template("users/login.html")
+    else:
+        register = request.form.get("register",None)
+        username = request.form.get("username")
+        password = request.form.get("password")
+        phone = request.form.get("phone")
+        address = request.form.get("address")
+        if register != None:
+            user = User(
+                username = username,
+                password = sha256_crypt.encrypt(password),
+                phone = phone,
+                address = address
+            )
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
 
+            return redirect("/users/dashboard")
